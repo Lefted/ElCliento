2a07bf5a72abbb9054d136f7b2b36963b57279ab
Pr3roxDLC <57639545+Pr3roxDLC@users.noreply.github.com>
2020-11-13 18:22:45 +0100

    Added Values to ESP and Chams

--- src/main/java/us/hemdgang/elcliento/modules/Chams.java
+++ src/main/java/us/hemdgang/elcliento/modules/Chams.java
@@ -7,8 +7,12 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.opengl.GL11;
 import us.hemdgang.elcliento.ElCliento;
+import us.hemdgang.elcliento.values.Value;
 
 public class Chams extends Module{
+
+    public Value<Boolean> shaded = new Value<Boolean>("shaded", true);
+
     public Chams() {
         super("Chams", Category.RENDER);
     }
@@ -28,6 +32,7 @@ public class Chams extends Module{
     public void onRenderLivingPre(RenderLivingEvent.Pre<? extends EntityLivingBase> event) {
         GL11.glEnable(GL11.GL_POLYGON_OFFSET_FILL);
         GlStateManager.enablePolygonOffset();
+        if(!shaded.getObject().booleanValue())GlStateManager.disableLighting();
         GlStateManager.doPolygonOffset(1.0F, -2000000);
 
     }
@@ -36,6 +41,7 @@ public class Chams extends Module{
     public void onRenderLivingPost(RenderLivingEvent.Post<? extends EntityLivingBase> event) {
         GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
         GlStateManager.doPolygonOffset(1.0F, 2000000);
+        if(!shaded.getObject().booleanValue())GlStateManager.enableLighting();
         GlStateManager.disablePolygonOffset();
     }
 
--- src/main/java/us/hemdgang/elcliento/modules/ESP.java
+++ src/main/java/us/hemdgang/elcliento/modules/ESP.java
@@ -7,8 +7,12 @@ import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import us.hemdgang.elcliento.utils.RenderUtils;
+import us.hemdgang.elcliento.values.Value;
 
 public class ESP extends Module {
+
+    public Value<Integer> mode = new Value<Integer>("mode", 0);
+
     public ESP() {
         super("ESP", Category.RENDER);
     }
@@ -24,14 +28,29 @@ public class ESP extends Module {
     }
 
     @SubscribeEvent
-    public void onWorldRenderLast(RenderWorldLastEvent e){
+    public void onWorldRenderLast(RenderWorldLastEvent e) {
 
-        for(Entity entity : mc.world.loadedEntityList) {
-            if(!(entity instanceof EntityPlayerSP)) {
-                RenderUtils.drawFlledCube(new AxisAlignedBB( entity.posX - entity.width / 2,  entity.posY,  entity.posZ - entity.width / 2,
-                        entity.posX - entity.width / 2 + entity.width,  entity.posY + entity.height,  entity.posZ - entity.width / 2 + entity.width), 3317416);
+        for (Entity entity : mc.world.loadedEntityList) {
+            if (!(entity instanceof EntityPlayerSP)) {
 
-                RenderUtils.drawOutlineBox((float) entity.posX - entity.width / 2, (float) entity.posY, (float) entity.posZ - entity.width / 2, entity.width, entity.height, entity.width, 1, 3317416);
+                switch (mode.getObject().intValue()) {
+                    case 0:
+                        RenderUtils.drawOutlineBox((float) entity.posX - entity.width / 2, (float) entity.posY, (float) entity.posZ - entity.width / 2, entity.width, entity.height, entity.width, 1, 3317416);
+                        break;
+                    case 1:
+                        RenderUtils.drawFlledCube(new AxisAlignedBB(entity.posX - entity.width / 2, entity.posY, entity.posZ - entity.width / 2,
+                                entity.posX - entity.width / 2 + entity.width, entity.posY + entity.height, entity.posZ - entity.width / 2 + entity.width), 3317416);
+                        break;
+                    default:
+                        RenderUtils.drawFlledCube(new AxisAlignedBB(entity.posX - entity.width / 2, entity.posY, entity.posZ - entity.width / 2,
+                                entity.posX - entity.width / 2 + entity.width, entity.posY + entity.height, entity.posZ - entity.width / 2 + entity.width), 3317416);
+
+                        RenderUtils.drawOutlineBox((float) entity.posX - entity.width / 2, (float) entity.posY, (float) entity.posZ - entity.width / 2, entity.width, entity.height, entity.width, 1, 3317416);
+                        break;
+
+
+                }
+
             }
         }
     }
